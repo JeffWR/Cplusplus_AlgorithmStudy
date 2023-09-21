e408f4f1ecc12ee82452804a8b92f57cc31f8e20
JeffWR <139133328+JeffWR@users.noreply.github.com>
2023-09-21 13:22:36 +0800

    Update P1303_High-precision multiplication (With Note).cpp

--- High-Precision-Algorithm/P1303_High-precision multiplication (With Note).cpp
+++ High-Precision-Algorithm/P1303_High-precision multiplication (With Note).cpp
@@ -22,27 +22,33 @@ int c[10000000]={0};
 int main(){
     cin >> arr1;
     cin >> arr2;
+    //Input the number in character form
     int len1=strlen(arr1);
     int len2=strlen(arr2);
     for(int i = 0 ; i < len1 ; i ++){
         a[i]=arr1[len1-i-1]-'0';
+        //Store the input string with individual digit
     }
     for(int i = 0 ; i < len2 ; i ++){
         b[i]=arr2[len2-i-1]-'0';
+        //Store the input string with individual digit
     }
     for(int i = 0 ; i < len1 ; i ++){
         for(int j = 0 ; j < len2 ; j ++){
-            c[i+j+1]+=(c[i+j]+a[i]*b[j])/10;
-            c[i+j]=(c[i+j]+a[i]*b[j])%10;
+            //Calculate each digit separately
+            c[i+j+1]+=(c[i+j]+a[i]*b[j])/10;//Number carry to the next digit
+            c[i+j]=(c[i+j]+a[i]*b[j])%10;//Number added to the current digit
         }
     }
     int Mlen=len1+len2;
     while(c[Mlen]==0&&Mlen!=0){
         Mlen--;
+        //Test the length of the finnial answer
     }
     for(int i = Mlen ; i >= 0; i -- ){
         cout << c[i];
     }
+    //Output
     cout << endl;
     return 0;
 }
